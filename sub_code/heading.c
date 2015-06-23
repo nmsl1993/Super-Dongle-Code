81f36eb1ea09803903c1640749e63872259baa6c
nmsl1993 <noahmslevy@gmail.com>
2015-06-22 23:27:42 -0700

    for some reason it's not looping through simulated data

--- sub_code/heading.c
+++ sub_code/heading.c
@@ -7,19 +7,19 @@
 #define SAMPLING_FREQUENCY 200e3
 typedef struct principal_frequency_t
 {
-    float frequency;
     int index;
-    float phase_noise;
+    float frequency;
+    float psd;
 }principal_frequency;
-
+principal_frequency calculate_pf(kiss_fft_cpx * carr, int carr_len);
 int main (void) {
   superdongle_packet_t spt __attribute__ ((aligned (__BIGGEST_ALIGNMENT__)));;
   kiss_fftr_cfg fft = kiss_fftr_alloc(CHANNEL_DEPTH,0, 0, 0); //128 input real valued fft, forward direction, let library allocate space
   kiss_fft_scalar rin[CHANNEL_DEPTH];
-  kiss_fft_cpx sout[CHANNEL_DEPTH/2 + 1];
+  kiss_fft_cpx cout[CHANNEL_DEPTH/2 + 1];
 
   printf("start loop \n");
-  while(loop((char *) &spt))
+  while(loop((char *) &spt) != 0)
   {
     int i;
     for(i = 0; i < CHANNEL_DEPTH; i++)
@@ -27,12 +27,38 @@ int main (void) {
         rin[i] = (kiss_fft_scalar) spt.data[3*i]; //This defaults to float which is correct...
     }
 
-    kiss_fftr(fft,rin,sout);
-
+    kiss_fftr(fft,rin,cout);
+    cout[0].r = 0;     //Zero meaningless DC component...
+    cout[0].i = 0;
+    principal_frequency pf = calculate_pf(cout,CHANNEL_DEPTH/2+1);
+    printf("%i,%f,%f\n",pf.index,pf.frequency,pf.psd);
 
   }
   printf("loop done\n");
   return 0;
 }
+principal_frequency calculate_pf(kiss_fft_cpx * carr, int carr_len)
+{
+    float mag_sq_sum = 0;
+    float mag_sq_max = 0;
+    int max_idx;
 
+    int i;
+    for(i = 0; i < carr_len; i++)
+    {
+         float sq_mag = carr[i].r * carr[i].r + carr[i].i * carr[i].i; 
+         mag_sq_sum += sq_mag;
+         if(sq_mag > mag_sq_max) 
+         {
+            max_idx = i;
+            mag_sq_max = sq_mag;
+         }
+    }
+    principal_frequency pf;
+    pf.frequency = (SAMPLING_FREQUENCY)*(float)max_idx/CHANNEL_DEPTH;
+    pf.index = max_idx;
+    pf.psd = mag_sq_max/mag_sq_sum;
+
+    return pf;
+}
  
