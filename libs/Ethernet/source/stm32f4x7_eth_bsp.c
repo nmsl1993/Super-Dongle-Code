60410928c8417b9137f9c4b5b2ba9d3b05b78c20
nmsl1993 <noahmslevy@gmail.com>
2015-07-12 18:22:31 -0700

    re enabled auto negotiation

--- libs/Ethernet/source/stm32f4x7_eth_bsp.c
+++ libs/Ethernet/source/stm32f4x7_eth_bsp.c
@@ -97,12 +97,12 @@ static void ETH_MACDMA_Config(void)
 
   /* Fill ETH_InitStructure parametrs */
   /*------------------------   MAC   -----------------------------------*/
-  //NOAH 100Mbps Hackz
-  
-  //ETH_InitStructure.ETH_AutoNegotiation = ETH_AutoNegotiation_Enable;
-  ETH_InitStructure.ETH_AutoNegotiation = ETH_AutoNegotiation_Disable;    
-  ETH_InitStructure.ETH_Speed = ETH_Speed_100M;
-  ETH_InitStructure.ETH_Mode = ETH_Mode_HalfDuplex;   
+    
+  ETH_InitStructure.ETH_AutoNegotiation = ETH_AutoNegotiation_Enable;
+  ///NOAH 100Mbps Hackz
+//ETH_InitStructure.ETH_AutoNegotiation = ETH_AutoNegotiation_Disable;    
+  //ETH_InitStructure.ETH_Speed = ETH_Speed_100M;
+  //ETH_InitStructure.ETH_Mode = ETH_Mode_HalfDuplex;   
 
   ETH_InitStructure.ETH_LoopbackMode = ETH_LoopbackMode_Disable;
   ETH_InitStructure.ETH_RetryTransmission = ETH_RetryTransmission_Disable;
