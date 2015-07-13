aeaefac76de20234c1f5aa6646a7afca0dcde078
nmsl1993 <noahmslevy@gmail.com>
2015-07-12 18:12:56 -0700

    added python script for setting sample rate

---
+++ udp_set_srate.py
@@ -0,0 +1,18 @@
+import socket
+import message_pb2
+UDP_IP = "10.0.0.111"
+UDP_PORT = 8898
+
+
+print "UDP target IP:", UDP_IP
+print "UDP target port", UDP_PORT
+
+com =  message_pb2.Command()
+com.SampleRateCommand.srate = int(raw_input("Enter sample rate"))
+
+print com
+
+msg = com.SerializeToString()
+print msg
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(msg,(UDP_IP,UDP_PORT))
